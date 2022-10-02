4ba894d65b22e8e67d8321feb430159357c721d8
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-02 23:57:13 +0900

    Feat: serializers.py, urls.py 추가

---
+++ backend/order/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = [
+            "user_id",
+            "product_id",
+            "address",
+            "receiver_name",
+            "total_price",
+            "delivery_fee",
+        ]
