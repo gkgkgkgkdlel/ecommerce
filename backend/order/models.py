df45be8e1064b07349a74354ddb0c63ebc2c8f95
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-02 23:56:26 +0900

    Fix: Order, OrderDetails 모델 수정

--- backend/order/models.py
+++ backend/order/models.py
@@ -9,11 +9,10 @@ from product.models import Product
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
     receiver_name = models.CharField(max_length=100)
-    total_price = models.IntegerField(default=0)
     delivery_fee = models.IntegerField(default=0)
+    total_price = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -28,5 +27,7 @@ class Payment(models.Model):
 
 class OrderDetails(models.Model):
     id = models.AutoField(primary_key=True)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     product_quantity = models.IntegerField(default=0)
+    product_total_price = models.IntegerField(default=0)
