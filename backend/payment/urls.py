0fcaf004384dc5046a6b1e57d237ff6a9969b798
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-03 19:42:31 +0900

    Feat: 결제 생성, 조회, 삭제 api 추가

--- backend/payment/urls.py
+++ backend/payment/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    # path("create/", views.CreateOrderView.as_view()),
-    # path("read/<int:order_id>/", views.ReadOrderView.as_view()),
-    # path("read/detail/<int:order_id>/", views.ReadOrderDetailView.as_view()),
-    # path("update/", views.UpdateOrderView.as_view()),
-    # path("delete/<int:order_id>/", views.DeleteOrderView.as_view()),
+    path("create/", views.CreatePaymentView.as_view()),
+    path("read/<int:payment_id>/", views.ReadPaymentView.as_view()),
+    path("delete/<int:payment_id>/", views.DeletePaymentView.as_view()),
 ]
--- backend/payment/views.py
+++ backend/payment/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+import json
+from .models import Payment
+from user.models import User
+from order.models import Order
+from .serializers import PaymentSerializer
 
-# Create your views here.
+
+@permission_classes((AllowAny,))
+class ReadPaymentView(APIView):
+    """
+    payment_id에 해당하는 결제 내역을 반환함.
+    """
+
+    def get(self, request, payment_id):
+        payment_obj = Payment.objects.get(id=payment_id)
+        pay_serializer = PaymentSerializer(payment_obj)
+
+        print("pay_serializer.data는 ", pay_serializer.data)
+
+        result = {"payment": pay_serializer.data}
+
+        return Response({"result": result}, status=status.HTTP_200_OK)
+
+
+@permission_classes((AllowAny,))
+class CreatePaymentView(APIView):
+    """
+    order_id에 해당하는 주문 내역을 결제함.
+    """
+
+    def post(self, request):
+        data = json.loads(request.body)
+        order_id = data["order_id"]
+        order_obj = Order.objects.get(id=order_id)
+        data["amount"] = order_obj.total_price
+
+        serializer = PaymentSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                {"message": "SUCCESS"}, status=status.HTTP_201_CREATED
+            )
+
+        else:
+            print(serializer.errors)
+
+            return Response(
+                {"message": "FAILED"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+@permission_classes((AllowAny,))
+class DeletePaymentView(APIView):
+    """
+    결제를 취소하는 api
+    """
+
+    def delete(self, request, payment_id):
+        payment_obj = Payment.objects.get(id=payment_id)
+        payment_obj.delete()
+
+        return Response({"message": "SUCCESS"}, status=status.HTTP_200_OK)
