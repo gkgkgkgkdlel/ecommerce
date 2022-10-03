3ea8fae8f6d384cddf761770a110d29bee544ee1
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-03 18:53:16 +0900

    Fix: Payment 클래스 위치 변경, amount로 컬럼명 변경

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from order.models import Order
 
 
 class Payment(models.Model):
     id = models.AutoField(primary_key=True)
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     method = models.CharField(max_length=100)
-    amout = models.IntegerField(default=0)
+    amount = models.IntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
-
-    class Meta:
-        managed = False
