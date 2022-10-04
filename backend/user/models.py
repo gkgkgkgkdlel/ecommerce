0ba7168ceaadb6f8b804cdcca9a58d64aa81c0b8
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-04 19:59:09 +0900

    Fix: User 모델 ReQUIRED_FIELDS 에러 수정

--- backend/user/models.py
+++ backend/user/models.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["email, name"]
+    REQUIRED_FIELDS = ["name"]
 
     def __str__(self):
         return self.email
